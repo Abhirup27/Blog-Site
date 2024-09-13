97a076e9532d47838cb8bf63b0ec43dadb6cdf04
abhirup27 <abhhirup27022001@outlook.com>
2024-09-13 16:08:19 +0530

    added styling and uuid

--- index.js
+++ index.js
@@ -1,18 +1,35 @@
 import express from "express";
 import bodyparser from "body-parser";
 import json from "json";
+import { v4 as uuid } from 'uuid';
+import session from "express-session";
+
+
 let users = {
         "users": [
-                {"username": "admin", "password":"1234"}
+                {"username": "admin", "password":"1234", posts:[]}
         ]
     }
 
 const app = express();
 const port = 3000;
+
+
+app.use(session(
+{ name:'SessionCookie',
+  genid: function(req) {
+      //console.log('session id created');
+    return uuid();}, 
+  secret: 'Shsh!Secret!',
+  resave: false,
+  saveUninitialized: false,
+  cookie: { secure: false,expires:60000 }
+    }));
 app.use(express.static("public"));
 app.use(bodyparser.urlencoded({ extended: true }));
 app.get("/", (req,res) =>
 {
+    console.log(req.sessionID)
     res.render("index.ejs");
 });
 app.get("/login", (req, res) =>
