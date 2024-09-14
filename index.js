63c1ac34c376804055947bd87895479748dc1b7f
Abhirup Bhattacharyya <abhirup27022001@outlook.com>
2024-09-14 20:40:47 +0530

    minor changes

--- index.js
+++ index.js
@@ -5,12 +5,19 @@ import { v4 as uuid } from 'uuid';
 import session from "express-session";
 
 
-let users = {
-        "users": [
-                {"username": "admin", "password":"1234", posts:[]}
-        ]
+let users = [
+    {
+        id: "1",
+        username: "admin",
+        password: "1234",
+        email: "admin@example.com",
+        lastLoginIp: "",
+        session: "",
+        posts: [{title:"TEST",content:"This is a sample text."}],
+        createdAt: "2024-09-14T12:00:00Z",
+        updatedAt: "2024-09-14T12:00:00Z"
     }
-
+];
 const app = express();
 const port = 3000;
 
@@ -27,15 +34,47 @@ app.use(session(
     }));
 app.use(express.static("public"));
 app.use(bodyparser.urlencoded({ extended: true }));
+
 app.get("/", (req,res) =>
 {
+    const ipAddress = getClientIp(req);
     console.log(req.sessionID)
-    res.render("index.ejs");
+    const user = users.find(u=>u.session == req.sessionID && u.lastLoginIp == ipAddress);
+    if (user) {
+        //res.send('Login successful!');
+        res.render("posts.ejs", {posts:user.posts});
+    }
+    else
+    {
+        res.render("index.ejs");
+    }
 });
-app.get("/login", (req, res) =>
-{
-    res.render("login.ejs");
+
+
+function getClientIp(req) {
+    return req.headers['x-forwarded-for']?.split(',').shift()
+      || req.socket?.remoteAddress;
+  }
+
+app.post('/login', (req, res) => {
+    const { username, password } = req.body;
+
+    // Find the user
+    let user = users.find(u => u.username === username && u.password === password);
+    req.session.username=req.sessionID;
+    console.log(req.sessionID)
+    const ipAddress = getClientIp(req);
+
+    if (user) {
+        user.lastLoginIp = ipAddress;
+        user.session = req.sessionID;
+        res.send('Login successful!');
+
+    } else {
+        res.status(401).send('Invalid username or password');
+    }
 });
+
 app.listen(port, () =>
 {
     console.log(`Server started at port ${port}`);
